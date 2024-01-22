a74454da476f0df7fc0c4d12d1b2bedb3f07d5a7
Mohamed El-Serafy <elserafy02@gmail.com>
2024-01-22 03:14:49 +0200

    partioningBy

--- Playground/src/playground/_21_reduction_operations_and_paralleism/simple_collection/SimpleCollection.java
+++ Playground/src/playground/_21_reduction_operations_and_paralleism/simple_collection/SimpleCollection.java
@@ -1,4 +1,4 @@
-package playground._21_reduction_operations_and_paralleism;
+package playground._21_reduction_operations_and_paralleism.simple_collection;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,6 +9,11 @@ public class SimpleCollection {
                 .stream()
                 .collect(Collectors.groupingBy(s -> s.length()))
                 .forEach((k, v) -> System.out.printf("%d character words are %s\n", k, v));
+
+        List.of("Fred", "Jim", "Sheila", "Bill", "Joe", "Sally", "John", "Tommy")
+                .stream()
+                .collect(Collectors.partitioningBy(s -> s.length() > 4))
+                .entrySet().forEach(System.out::println);
 
     }
 }
