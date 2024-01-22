1736b231904b8b0e40764f1f910031afe4339e22
Mohamed El-Serafy <elserafy02@gmail.com>
2024-01-22 04:14:57 +0200

    quick question

---
+++ Playground/src/playground/_21_reduction_operations_and_paralleism/parallel_stream_operation/quick_question/parallel_counter/App.java
@@ -0,0 +1,14 @@
+package playground._21_reduction_operations_and_paralleism.parallel_stream_operation.quick_question.parallel_counter;
+
+import java.util.stream.LongStream;
+
+public class App {
+    public static void main(String[] args) {
+        long[] counter = { 0 };
+        LongStream.range(0, 10_000_000_000L)
+                .parallel().peek(x -> counter[0]++) // not thread safe
+                .allMatch(x -> x >= 0);
+
+        System.out.println(counter[0]);
+    }
+}
