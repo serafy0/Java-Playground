d39a67920191df87bde380f7e6617fe42dfbc034
Mohamed El-Serafy <elserafy02@gmail.com>
2024-01-22 03:45:09 +0200

    unused import

--- Playground/src/playground/_21_reduction_operations_and_paralleism/downstream_operations/downstream_collecting/DownstreamCollecting.java
+++ Playground/src/playground/_21_reduction_operations_and_paralleism/downstream_operations/downstream_collecting/DownstreamCollecting.java
@@ -1,7 +1,6 @@
 package playground._21_reduction_operations_and_paralleism.downstream_operations.downstream_collecting;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
