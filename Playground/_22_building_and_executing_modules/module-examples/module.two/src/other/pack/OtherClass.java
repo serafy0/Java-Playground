646e0212a55fb21819444a0ae2457e8556c462ed
Mohamed El-Serafy <elserafy02@gmail.com>
2024-01-26 09:06:13 +0200

    module examples

---
+++ Playground/_22_building_and_executing_modules/module-examples/module.two/src/other/pack/OtherClass.java
@@ -0,0 +1,7 @@
+package other.pack;
+
+public class OtherClass {
+    public static void main(String[] args) {
+        System.out.println("OtherClass");
+    }
+}
--- Playground/src/playground/_21_reduction_operations_and_paralleism/question_deep_dive/App.java
+++ Playground/src/playground/_21_reduction_operations_and_paralleism/question_deep_dive/App.java
@@ -2,7 +2,6 @@ package playground._21_reduction_operations_and_paralleism.question_deep_dive;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 class Student {
